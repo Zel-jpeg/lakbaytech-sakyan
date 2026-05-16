b2e94c641e7a0827ba0e4c9c787aa0499febef5b
Sakyan Dev <villanueva.azel@dnsc.edu.ph>
2026-05-16 11:27:40 +0800

    more banner update

--- sakyan-frontend/src/components/cars/FeaturedBanner.jsx
+++ sakyan-frontend/src/components/cars/FeaturedBanner.jsx
@@ -63,9 +63,9 @@ function CarThumbnails({ cars = [] }) {
       <div className="relative w-full max-w-[280px] sm:max-w-[320px] h-full">
         {cars.map((car, i) => {
           const transforms = [
-            'rotate-[-6deg] translate-x-[-10%] translate-y-[4%] z-10',
-            'rotate-[0deg] translate-x-[5%] translate-y-[-6%] z-20',
-            'rotate-[6deg] translate-x-[20%] translate-y-[4%] z-10',
+            'rotate-[-6deg] translate-x-[-10%] translate-y-[-8%] z-10',
+            'rotate-[0deg] translate-x-[5%] translate-y-[-18%] z-20',
+            'rotate-[6deg] translate-x-[20%] translate-y-[-8%] z-10',
           ]
           return (
             <div key={car.id}
